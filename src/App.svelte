a978df04ba837172eddcd7d49ab1c32deed13f2a
robbytrujillo <robbyikusuma88@gmail.com>
2025-05-22 06:04:22 +0700

    communication between components

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,13 @@
 	setTimeout(function () {
 		title = "Donation";
 	}, 2000);
+
+	// definisikan state apa yang mau dikirimkan
+	let charities = [
+		'Charity 1',
+		'Charity 2',
+		'Charity 3'
+	];
 </script>
 
 <style>
@@ -13,13 +20,18 @@ h1 {
 	color: #e2071c;
 }	
 
-:global(div) {
+/* :global(div) { */
+div {
 	border: 0.5em solid #1a1a1a;
 	padding: 1em;
 }
 </style>
 
+<svelte:head>
+	<link rel="stylesheet" href="https://cdn.rawgit.com/kimeiga/bahunya/css/bahunya-0.1.3.css">
+</svelte:head>
+
 <div>
 	<h1>{title}</h1>
-	<CharityList />
+	<CharityList title={title} charities={charities} />
 </div>
