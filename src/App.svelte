e72edfe9ab243f2bb7f7010e2261fd1acd14e423
robbytrujillo <robbyikusuma88@gmail.com>
2025-05-21 19:49:41 +0700

    writing css in svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,25 @@
 <script>
+	import CharityList from "./CharityList.svelte";
+
 	let title = "Charity";
 	setTimeout(function () {
 		title = "Donation";
 	}, 2000);
 </script>
 
-<h1>{title}</h1>
-
 <style>
 h1 {
-	fonst-size: 3em;
+	font-size: 3em;
 	color: #e2071c;
 }	
+
+:global(div) {
+	border: 0.5em solid #1a1a1a;
+	padding: 1em;
+}
 </style>
+
+<div>
+	<h1>{title}</h1>
+	<CharityList />
+</div>
---
+++ src/CharityList.svelte
@@ -0,0 +1,8 @@
+<div>
+    <h2>Daftar Charity</h2>
+    <ul>
+        <li>Charity 1</li>
+        <li>Charity 2</li>
+        <!-- <li>Charity 3</li> -->
+    </ul>
+</div>
