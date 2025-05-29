46567be6e50ce2f92027fc0d83230c331ac1dd92
robbytrujillo <robbyikusuma88@gmail.com>
2025-05-30 03:42:28 +0700

    Using a Routing Library

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,21 @@
 <script>
+	// install page.js
+	import router from "page";
+
 	import Home from "./pages/Home.svelte";
 	import About from "./pages/About.svelte";
 	import Contact from "./pages/Contact.svelte";
+
+	let page;
+
+	router("/", () => { page = Home; });
+	router("/about", () => { page = About; });
+	router("/contact", () => { page = Contact; });
+
+	router.start();
 </script>
 
 <!-- <Home /> -->
 <!-- <About /> -->
-<Contact />
+<!-- <Contact /> -->
+<svelte:component this={page} />
